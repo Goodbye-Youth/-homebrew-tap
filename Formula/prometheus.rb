60dd89fce015ea4d860e7ad7f8c1069446b1f2b2
百里 <baili@ywwl.com>
2023-05-17 17:22:48 +0800

    add prometheus.rb

---
+++ Formula/prometheus.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HomebrewTap < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Goodbye-Youth/homebrew-tap/releases/download/prometheus-2.44.0/prometheus-2.44.0.darwin-amd64.tar.gz"
+  sha256 "05b100f105cbb664e99d12361cbc938318ee99a1f8c4d5b9c7dbbfd2c07923c3"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-tap`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
