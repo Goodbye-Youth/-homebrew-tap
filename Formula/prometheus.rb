b206d910579e9a62f17195bd184a0d9d8a36d776
GeneXu <36945511+Goodbye-Youth@users.noreply.github.com>
2023-05-17 17:49:15 +0800

    Update prometheus.rb

--- Formula/prometheus.rb
+++ Formula/prometheus.rb
@@ -9,4 +9,36 @@ class Prometheus < Formula
     bin.install "prometheus"
     etc.install "prometheus.yml"
   end
+  
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+          </dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/prometheus</string>
+            <string>-config</string>
+            <string>#{etc}/prometheus.yml</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/prometheus.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/prometheus.log</string>
+        </dict>
+      </plist>
+    EOS
+  end
 end
